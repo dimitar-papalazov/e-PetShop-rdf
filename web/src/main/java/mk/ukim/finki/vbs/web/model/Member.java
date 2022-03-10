b75a89491b932b73b42119af67810e05aca2ce2e
Dimitar Papalazov <dimitar.papalazov@students.finki.ukim.mk>
2022-03-10 20:21:32 +0100

    Fix & add member getters

--- web/src/main/java/mk/ukim/finki/vbs/web/model/Member.java
+++ web/src/main/java/mk/ukim/finki/vbs/web/model/Member.java
@@ -44,7 +44,7 @@ public class Member implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
@@ -70,5 +70,21 @@ public class Member implements UserDetails {
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public MemberRole getRole() {
+        return role;
     }
 }
